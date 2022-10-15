2d15c18cc417c704593ef9c2f42b8ac023ae2644
liej6799 <52598803+liej6799@users.noreply.github.com>
2022-10-15 10:07:19 +0800

    added common logic

--- dmonitoring.py
+++ dmonitoring.py
@@ -3,10 +3,12 @@ import numpy as np
 import onnx
 importlib
 import onnxruntime as rt
+import cv2
+import torch
+from utils import bgr_to_yuv, transform_frames, printf, FULL_FRAME_SIZE, create_image_canvas, PATH_TO_CACHE  # noqa
 
-MODEL_WIDTH = 1440;
-MODEL_HEIGHT = 960;
-
+plot_img_height, plot_img_width = 480, 640 # can be reduced for wandb
+seq_len = 1
 #load fake image
 def get_random_input_tensors():
   np_inputs = {
@@ -14,6 +16,29 @@ def get_random_input_tensors():
     'calib': np.zeros((1, 3), dtype=np.float32),
   }
   return np_inputs
+
+
+    #return imgs_med_model
+def get_real_input():
+    img = cv2.imread("sample/135.png")
+    img = cv2.resize(img, (1164, 874), interpolation = cv2.INTER_AREA)
+ 
+    rgb_frames = np.zeros((seq_len, plot_img_height, plot_img_width, 3), dtype=np.uint8)
+    yuv_frames = np.zeros((seq_len + 1, FULL_FRAME_SIZE[1]*3//2, FULL_FRAME_SIZE[0]), dtype=np.uint8)
+    stacked_frames = np.zeros((seq_len, 12, 128, 256), dtype=np.uint8)
+
+    yuv_frames = np.zeros((1, FULL_FRAME_SIZE[1]*3//2, FULL_FRAME_SIZE[0]), dtype=np.uint8)
+   
+    yuv_frame2 = bgr_to_yuv(img)
+    yuv_frames[0] = yuv_frame2
+    prepared_frames = transform_frames(yuv_frames)
+
+    return torch.from_numpy(prepared_frames).float()
+    #cv2.imshow('awd', img)
+    #cv2.waitKey(0) 
+
+
+
 
 def load_inference_model(path_to_model):
 
@@ -29,12 +54,27 @@ def load_inference_model(path_to_model):
 
 
 # run on the model
-onnx_model, run_model = load_inference_model("models/dmonitoring_model.onnx")
+onnx_model, run_model = load_inference_model("models/supercombo.onnx")
 
 #inference on the model
-inputs = get_random_input_tensors()
-print(inputs["input_img"])
-print(inputs["calib"])
-outs = run_model(inputs)
-print(outs.shape)
+# inputs = get_random_input_tensors()
+# print(inputs["input_img"])
+# print(inputs["calib"])
+# outs = run_model(inputs)
+# print(outs.shape)
 
+recurrent_state = np.zeros((1, 512), dtype=np.float32)
+input_frame = get_real_input()
+print(input_frame[0:1].numpy().astype(np.float32).shape)
+
+for t_idx in range(seq_len):
+    inputs = {
+        'input_imgs': input_frame[t_idx:t_idx+1].numpy().astype(np.float32),
+        'big_input_imgs': input_frame[t_idx:t_idx+1].numpy().astype(np.float32),
+        
+        'desire': np.zeros((1, 8), dtype=np.float32),
+        'traffic_convention': np.array([0, 1], dtype=np.float32).reshape(1, 2),
+        'initial_state': recurrent_state,
+    }
+
+    outs, recurrent_state = run_model(inputs)
